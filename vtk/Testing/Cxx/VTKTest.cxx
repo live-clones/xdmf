3ca06d31f1da5be383179337bd927feb860fbfe6
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-04-14 10:43:02 -0400

    Add Extra argument, so we can test CTH datasets

--- vtk/Testing/Cxx/VTKTest.cxx
+++ vtk/Testing/Cxx/VTKTest.cxx
@@ -17,28 +17,35 @@ int main(int argc, char* argv[])
   cout << "------------- UpdateInformation --------" << endl;
   vr->UpdateInformation();
 
-  vtkDataArraySelection* ds = vr->GetCellDataArraySelection();
-  ds->DisableAllArrays();
-  ds->EnableArray(ds->GetArrayName(0));
-  vr->SetStride(2, 3, 4);
-  cout << "------------- Update -------------------" << endl;
-  vr->Update();
-  cout << "------------- Reader: ------------------" << endl;
-  //vr->Print(cout);
-  cout << "------------- Output: ------------------" << endl;
-  cout << "Number of cells: " << vr->GetOutput()->GetNumberOfCells() << endl;
-  //vr->GetOutput()->Print(cout);
-  vtkRectilinearGrid* rg = vtkRectilinearGrid::SafeDownCast( vr->GetOutput() );
-  if ( rg )
+  if ( argc > 2 )
     {
-    vtkDataArray* array = rg->GetCellData()->GetArray(0);
-    //array->Print(cout);
-    cout << "Number of tuples of array: " << array->GetNumberOfTuples() << endl;
-    cout << "Number of X coords: " << rg->GetXCoordinates()->GetNumberOfTuples() << endl;
-    cout << "Number of y coords: " << rg->GetYCoordinates()->GetNumberOfTuples() << endl;
-    cout << "Number of z coords: " << rg->GetZCoordinates()->GetNumberOfTuples() << endl;
+    vtkDataArraySelection* ds = vr->GetCellDataArraySelection();
+    ds->DisableAllArrays();
+    ds->EnableArray(ds->GetArrayName(0));
+    vr->SetStride(2, 3, 4);
+    cout << "------------- Update -------------------" << endl;
+    vr->Update();
+    cout << "------------- Reader: ------------------" << endl;
+    //vr->Print(cout);
+    cout << "------------- Output: ------------------" << endl;
+    cout << "Number of cells: " << vr->GetOutput()->GetNumberOfCells() << endl;
+    //vr->GetOutput()->Print(cout);
+    vtkRectilinearGrid* rg = vtkRectilinearGrid::SafeDownCast( vr->GetOutput() );
+    if ( rg )
+      {
+      vtkDataArray* array = rg->GetCellData()->GetArray(0);
+      //array->Print(cout);
+      cout << "Number of tuples of array: " << array->GetNumberOfTuples() << endl;
+      cout << "Number of X coords: " << rg->GetXCoordinates()->GetNumberOfTuples() << endl;
+      cout << "Number of y coords: " << rg->GetYCoordinates()->GetNumberOfTuples() << endl;
+      cout << "Number of z coords: " << rg->GetZCoordinates()->GetNumberOfTuples() << endl;
+      }
+    cout << "------------- Delete  ------------------" << endl;
     }
-  cout << "------------- Delete  ------------------" << endl;
+  else
+    {
+    vr->Print(cout);
+    }
   vr->Delete();
   return 0;
 }
