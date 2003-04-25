b11bf0f7d31f05c2f8376a96ffba7c82d6c553ac
Jerry Clarke <clarke@arl.army.mil>
2003-04-25 13:24:48 -0400

    fix to build XDMF with NDGM properly

--- vtk/vtkXdmfRenderWindowInteractor.h
+++ vtk/vtkXdmfRenderWindowInteractor.h
@@ -22,23 +22,13 @@
 /*     for more information.                                       */
 /*                                                                 */
 /*******************************************************************/
-#if !defined CYGWIN
 #include <vtkXRenderWindowInteractor.h>
 
 class VTK_EXPORT vtkXdmfRenderWindowInteractor : public vtkXRenderWindowInteractor
-#else
-#include <vtkWin32RenderWindowInteractor.h>
-
-class VTK_EXPORT vtkXdmfRenderWindowInteractor : public vtkWin32RenderWindowInteractor
-#endif
 {
 public:
   static vtkXdmfRenderWindowInteractor *New();
-#if !defined CYGWIN
   vtkTypeMacro(vtkXdmfRenderWindowInteractor,vtkXRenderWindowInteractor);
-#else
-  vtkTypeMacro(vtkXdmfRenderWindowInteractor,vtkWin32RenderWindowInteractor);
-#endif
   virtual void LoopOnce();
   virtual void Start( int Block = 0 );
 };
