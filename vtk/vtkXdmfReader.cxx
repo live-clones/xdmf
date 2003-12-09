9994d4f4ce6deec334ebb16ec0bf85af60ffc4ac
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-12-09 16:59:42 -0500

    ERR: Delete array with the proper operator

--- vtk/vtkXdmfReader.cxx
+++ vtk/vtkXdmfReader.cxx
@@ -73,7 +73,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <vector>
 
 vtkStandardNewMacro(vtkXdmfReader);
-vtkCxxRevisionMacro(vtkXdmfReader, "1.30");
+vtkCxxRevisionMacro(vtkXdmfReader, "1.31");
 
 #if defined(_WIN32) && (defined(_MSC_VER) || defined(__BORLANDC__))
 #  include <direct.h>
@@ -556,7 +556,8 @@ void vtkXdmfReader::Execute()
             } 
           else 
             {
-            XdmfFloat64   *TmpPp, *TmpPoints = new XdmfFloat64[ Length ];
+            XdmfFloat64   *TmpPp;
+            XdmfFloat64   *TmpPoints = new XdmfFloat64[ Length ];
             XdmfInt64     ii, jj, kk;
 
             Geometry->GetPoints()->GetValues( 0, TmpPoints, Length );
@@ -610,7 +611,7 @@ void vtkXdmfReader::Execute()
                 cnt ++;
                 }
               }
-            delete TmpPoints;
+            delete [] TmpPoints;
             }
           Points->Modified();
           Pointset->Modified();
