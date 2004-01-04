7d45a29bd6d177956bd283661f1ec53b7e4db33a
Andy Cedilnik <andy.cedilnik@kitware.com>
2004-01-04 12:57:57 -0500

    ENH: Add support for image data

--- vtk/Testing/Cxx/TestWriters.cxx
+++ vtk/Testing/Cxx/TestWriters.cxx
@@ -6,12 +6,42 @@
 #include "vtkIdList.h"
 #include "vtkCellData.h"
 #include "vtkPointData.h"
+#include "vtkImageNoiseSource.h"
+#include "vtkImageData.h"
 
 #define OutVector3(x) (x)[0] << "," << (x)[1] << "," << (x)[2]
 #define ABS(x) (((x)<0)?-(x):(x))
+#define MAX(x,y) (((x)>(y))?(x):(y))
+
+template<class D1, class D2, class DiffType>
+int CompareTwoSimilarItems(D1 i1, D2 i2, DiffType diff)
+{
+  if ( diff == 0 )
+    {
+    return i1 == i2;
+    }
+  DiffType rdiff = ABS(i1 - i2);
+  if ( rdiff < (diff * .00001) )
+    {
+    return 1;
+    }
+  return 0;
+}
+
+template<class D1, class D2>
+int CompareTwoItems(D1 i1, D2 i2)
+{
+  return i1 == i2;
+}
 
 #define TestCompareTwoItems(x, y, str) \
-  if ( (x) != (y) ) \
+  if ( !CompareTwoItems((x), (y)) ) \
+    { \
+    cerr << __LINE__ << " " << str << " " << (x) << " <> " << (y) << endl; \
+    return 0; \
+    }
+#define TestCompareTwoSimilarItems(x, y, str) \
+  if ( !CompareTwoSimilarItems((x), (y), MAX(ABS(x),ABS(y))) ) \
     { \
     cerr << __LINE__ << " " << str << " " << (x) << " <> " << (y) << endl; \
     return 0; \
@@ -19,12 +49,12 @@
 
 template<class InputDataType, class OutputDataType>
 OutputDataType* TestWriter(InputDataType* input, const char* fname, 
-  OutputDataType*)
+  OutputDataType*, int alllight)
 {
   vtkXdmfWriter *writer = vtkXdmfWriter::New();
   writer->SetFileName(fname);
   writer->SetInput(input);
-  //writer->AllLightOn();
+  writer->SetAllLight(alllight);
   writer->Write();
   writer->Delete();
 
@@ -46,43 +76,60 @@ OutputDataType* TestWriter(InputDataType* input, const char* fname,
   return dt;
 }
 
-int CompareFieldData(vtkFieldData* da, vtkFieldData* db)
+int CompareArrays(vtkDataArray* daArray, vtkDataArray *dbArray)
 {
-  TestCompareTwoItems(da->GetNumberOfArrays(),
-    db->GetNumberOfArrays(),
-    "Number of arrays is not the same");
-  vtkIdType array, tuple, component;
-  for ( array = 0; array < da->GetNumberOfArrays(); array ++ )
+  vtkIdType tuple, component;
+  TestCompareTwoItems(daArray->GetNumberOfTuples(),
+    dbArray->GetNumberOfTuples(),
+    "Number of tuples is not the same");
+  TestCompareTwoItems(daArray->GetNumberOfComponents(),
+    dbArray->GetNumberOfComponents(),
+    "Number of components is not the same");
+  for ( tuple = 0; tuple < daArray->GetNumberOfTuples(); tuple ++ )
     {
-    const char* name = da->GetArrayName(array);
-    vtkDataArray* daArray = da->GetArray(name);
-    vtkDataArray* dbArray = db->GetArray(name);
-    if ( !daArray || !dbArray )
+    for ( component = 0; component < daArray->GetNumberOfComponents(); component ++ )
       {
-      cerr << "Cannot find array named " << name << " (" << daArray  << " " << dbArray << ")" << endl;
-      return 0;
-      }
-    TestCompareTwoItems(daArray->GetNumberOfTuples(),
-      dbArray->GetNumberOfTuples(),
-      "Number of tuples is not the same");
-    TestCompareTwoItems(daArray->GetNumberOfComponents(),
-      dbArray->GetNumberOfComponents(),
-      "Number of components is not the same");
-    for ( tuple = 0; tuple < daArray->GetNumberOfTuples(); tuple ++ )
-      {
-      for ( component = 0; component < daArray->GetNumberOfComponents(); component ++ )
-        {
-        double daVal = daArray->GetComponent(tuple, component);
-        double dbVal = dbArray->GetComponent(tuple, component);
-        TestCompareTwoItems(daVal, dbVal,
-          "Value for tuple " << tuple << " component " << component << " are not the same");
-        }
+      double daVal = daArray->GetComponent(tuple, component);
+      double dbVal = dbArray->GetComponent(tuple, component);
+      TestCompareTwoSimilarItems(daVal, dbVal,
+        "Value for tuple " << tuple << " component " << component << " are not the same");
       }
     }
   return 1;
 }
 
-int CompareDataSets(vtkPolyData* pd, vtkUnstructuredGrid* ug)
+int CompareFieldData(vtkFieldData* da, vtkFieldData* db)
+{
+  TestCompareTwoItems(da->GetNumberOfArrays(),
+    db->GetNumberOfArrays(),
+    "Number of arrays is not the same");
+  vtkIdType array;
+  cout << "Found " << da->GetNumberOfArrays() << " arrays" << endl;
+  for ( array = 0; array < da->GetNumberOfArrays(); array ++ )
+    {
+    const char* name = da->GetArrayName(array);
+    vtkDataArray* daArray = da->GetArray(name);
+    vtkDataArray* dbArray = db->GetArray(name);
+    if ( !name )
+      {
+      daArray = da->GetArray(array);
+      dbArray = db->GetArray(array);
+      }
+    if ( !daArray || !dbArray )
+      {
+      cerr << "Cannot find array named " << name << " (" << daArray  << " " << dbArray << ")" << endl;
+      return 0;
+      }
+    int res = ::CompareArrays(daArray, dbArray);
+    if ( !res )
+      {
+      return 0;
+      }
+    }
+  return 1;
+}
+
+int CompareDataSets(vtkDataSet* pd, vtkDataSet* ug)
 {
   int pdNumCells = pd->GetNumberOfCells();
   int ugNumCells = ug->GetNumberOfCells();
@@ -106,7 +153,7 @@ int CompareDataSets(vtkPolyData* pd, vtkUnstructuredGrid* ug)
     diff[2] = pdPoint[2] - ugPoint[2];
     for ( kk = 0; kk < 3; kk ++ )
       {
-      TestCompareTwoItems(pdPoint[kk], ugPoint[kk],
+      TestCompareTwoSimilarItems(pdPoint[kk], ugPoint[kk],
         "Points " << cc << " are not the same: " << OutVector3(pdPoint) << " <> " << OutVector3(ugPoint) << endl << "Difference: " << OutVector3(diff));
       }
     }
@@ -130,16 +177,62 @@ int CompareDataSets(vtkPolyData* pd, vtkUnstructuredGrid* ug)
   pdCellPts->Delete();
   ugCellPts->Delete();
 
+  cout << "Point arrays" << endl;
   if ( !CompareFieldData(pd->GetPointData(), ug->GetPointData()) )
     {
     return 0;
     }
+  cout << "Cell arrays" << endl;
   if ( !CompareFieldData(pd->GetCellData(), ug->GetCellData()) )
     {
     return 0;
     }
 
   return 1;
+}
+
+int TestPolyData(vtkPolyData* pd, const char* filename, int alllight)
+{
+  cout << "** Preform PolyData test" << endl;
+  vtkUnstructuredGrid* ug 
+    = ::TestWriter(pd, filename, (vtkUnstructuredGrid*)0,
+      alllight);
+  if ( !ug )
+    {
+    return 1;
+    }
+
+  int res = CompareDataSets(pd, ug);
+
+  ug->Delete();
+  if ( !res )
+    {
+    cerr << "Errors while running poly data test" << endl;
+    return 1;
+    }
+  return 0;
+}
+
+template<class DataType>
+int TestSimpleData(DataType* orData, const char* filename, int alllight)
+{
+  cout << "** Preform simple data test" << endl;
+  DataType* rdData = ::TestWriter(orData, filename, (DataType*)0,
+      alllight);
+  if ( !rdData )
+    {
+    return 1;
+    }
+
+  int res = CompareDataSets(orData, rdData);
+
+  rdData->Delete();
+  if ( !res )
+    {
+    cerr << "Errors while running poly data test" << endl;
+    return 1;
+    }
+  return 0;
 }
 
 int main(int argc, char* argv[])
@@ -149,21 +242,22 @@ int main(int argc, char* argv[])
     cout << "Usage: " << argv[0] << " <xdmf file>" << endl;
     return 0;
     }
+  int res = 0;
 
   vtkSphereSource *sphere = vtkSphereSource::New();
-  vtkUnstructuredGrid* ug 
-    = ::TestWriter(sphere->GetOutput(), argv[1], (vtkUnstructuredGrid*)0);
-  if ( !ug )
-    {
-    sphere->Delete();
-    return 1;
-    }
-
-  int res = CompareDataSets(sphere->GetOutput(), ug);
-
-  ug->Delete();
+  res += ::TestPolyData(sphere->GetOutput(), argv[1], 0);
+  res += ::TestPolyData(sphere->GetOutput(), argv[1], 1);
   sphere->Delete();
-  if ( !res )
+
+  vtkImageNoiseSource* noise= vtkImageNoiseSource::New();
+  noise->SetWholeExtent(0, 5, 0, 12, 0, 17);
+  noise->Update();
+  res += ::TestSimpleData(noise->GetOutput(), argv[1], 1);
+  res += ::TestSimpleData(noise->GetOutput(), argv[1], 0);
+  noise->Delete();
+
+
+  if ( res )
     {
     cerr << "Errors while running test" << endl;
     return 1;
