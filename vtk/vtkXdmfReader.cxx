e52143e539c004be04beb06166a89b767d249883
Berk Geveci <berk.geveci@kitware.com>
2006-10-04 11:04:26 -0400

    COMP: Fixed unused variable warning

--- vtk/vtkXdmfReader.cxx
+++ vtk/vtkXdmfReader.cxx
@@ -87,7 +87,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define USE_IMAGE_DATA // otherwise uniformgrid
 
 vtkStandardNewMacro(vtkXdmfReader);
-vtkCxxRevisionMacro(vtkXdmfReader, "1.71");
+vtkCxxRevisionMacro(vtkXdmfReader, "1.72");
 
 vtkCxxSetObjectMacro(vtkXdmfReader,Controller,vtkMultiProcessController);
 
@@ -2782,8 +2782,6 @@ void vtkXdmfReader::UpdateGrids()
 int vtkXdmfReader::FillOutputPortInformation(int port,
                                              vtkInformation *info)
 { 
-  vtkInformation* outInfo =
-    this->GetExecutive()->GetOutputInformation()->GetInformationObject(port);
   info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkDataObject");
   return 1;
 }
