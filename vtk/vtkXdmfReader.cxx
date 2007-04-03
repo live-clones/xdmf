d57474f4aff1222d9934e046a4532ec5be6c1ddd
Jerry Clarke <clarke@arl.army.mil>
2007-04-03 16:28:19 -0400

    Support Old style Collections

--- vtk/vtkXdmfReader.cxx
+++ vtk/vtkXdmfReader.cxx
@@ -86,7 +86,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define USE_IMAGE_DATA // otherwise uniformgrid
 
 vtkStandardNewMacro(vtkXdmfReader);
-vtkCxxRevisionMacro(vtkXdmfReader, "1.9");
+vtkCxxRevisionMacro(vtkXdmfReader, "1.10");
 
 vtkCxxSetObjectMacro(vtkXdmfReader,Controller,vtkMultiProcessController);
 
@@ -2892,9 +2892,11 @@ void vtkXdmfReader::UpdateGrids()
     }else{
         // It's a Uniform Grid
         // All grids are treated the same so make it a collection
-        if(collName) delete [] collName;
-        collName=new char[strlen(gridName)+1]; 
-        strcpy(collName,  gridName);
+        // if(collName) delete [] collName;
+        if(!collName){
+            collName=new char[strlen(gridName)+1]; 
+            strcpy(collName,  gridName);
+            }
         this->UpdateUniformGrid(gridNode, collName);
     }
 
