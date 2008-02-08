1c530d482d4369112a3d6681a75858d4a4f51b30
Jerry Clarke <clarke@arl.army.mil>
2008-02-08 10:25:42 -0500

    Change some comments and fix Release() method

--- libsrc/XdmfObject.h
+++ libsrc/XdmfObject.h
@@ -32,8 +32,8 @@
 * The Data Format utilizes
 * the Hierarchical Data Format Version 5 (HDF5) , from NCSA, to store large amount of data
 * that can be accessed in a host independent fashion. The eXtensible Markup Language
-* (XML) is used to describe the high level organization of the data in HDF5. Finally, Network
-* Distributed Global Memory (NDGM) is used to allow distributed access to cooperating
+* (XML) is used to describe the high level organization of the data in HDF5. Finally
+* a built-in Distributed Shared Memory (DSM) system is used to allow distributed access to cooperating
 * applications.
 
 * Access to XDMF is provided via a C++ class library. 
