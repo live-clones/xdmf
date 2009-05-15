6d0464b85f2aa8dd87548138ae3bf980c430629e
Bill Hoffman <bill.hoffman@kitware.com>
2009-05-15 15:40:32 -0400

    ENH: add dashboard support

---
+++ CTestConfig.cmake
@@ -0,0 +1,13 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "Xdmf")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "hsai-web-origin.arl.army.mil")
+set(CTEST_DROP_LOCATION "/hsai/CDash/submit.php?project=Xdmf")
+set(CTEST_DROP_SITE_CDASH TRUE)
