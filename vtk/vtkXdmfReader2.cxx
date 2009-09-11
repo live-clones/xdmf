288fca2f9ee2ea595ca88ea1a5aef2be369f0083
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-09-11 17:01:16 -0400

    PERF: If the number of blocks exceeds 1000, then the SIL is no longer built,   only top-level grids information is passed on to the client, thus improving   performance when the xmf file is HUGE.
    ENH: scalar grid-centered attributes are now treated as categories in the SIL
    and user can select blocks using the grid-centered attribute values.

--- vtk/vtkXdmfReader2.cxx
+++ vtk/vtkXdmfReader2.cxx
@@ -88,7 +88,7 @@ private:
 vtkStandardNewMacro(vtkXdmfReader2Tester);
 
 vtkStandardNewMacro(vtkXdmfReader2);
-vtkCxxRevisionMacro(vtkXdmfReader2, "1.5");
+vtkCxxRevisionMacro(vtkXdmfReader2, "1.6");
 //----------------------------------------------------------------------------
 vtkXdmfReader2::vtkXdmfReader2()
 {
@@ -206,7 +206,13 @@ bool vtkXdmfReader2::PrepareDocument()
     {
     this->XdmfDocument->SetActiveDomain(static_cast<int>(0));
     }
-  this->SILUpdateStamp++;
+
+  if (this->XdmfDocument->GetActiveDomain() &&
+    this->XdmfDocument->GetActiveDomain()->GetSIL()->GetMTime() >
+    this->GetMTime())
+    {
+    this->SILUpdateStamp++;
+    }
 
   this->LastTimeIndex = 0; // reset time index when the file changes.
   return (this->XdmfDocument->GetActiveDomain() != 0);
