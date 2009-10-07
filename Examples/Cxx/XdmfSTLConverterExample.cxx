5c10155aeba5795a313bb7fa98c0d9ff1352afb4
Kenneth Leiter <kenneth.leiter@arl.army.mil>
2009-10-07 15:52:35 -0400

    ENH: Allow user to specify where to pass heavyName and a parent element to attach written data to

--- Examples/Cxx/XdmfSTLConverterExample.cxx
+++ Examples/Cxx/XdmfSTLConverterExample.cxx
@@ -1,4 +1,4 @@
-#include "XdmfSTLConverter.txx"
+#include "XdmfSTLConverter.cxx"
 
 #include <XdmfDOM.h>
 #include <XdmfDomain.h>
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include <map>
+#include <set>
 
 int main( int argc, const char* argv[] )
 {
@@ -13,15 +14,15 @@ int main( int argc, const char* argv[] )
 	 * Create an STL vector and map and write out to XDMF
 	 */
 
-	XdmfSTLConverter * mySerializer = new XdmfSTLConverter();
-	XdmfDOM * myDOM = new XdmfDOM();
+	XdmfSTLConverter mySerializer = XdmfSTLConverter();
+	XdmfDOM myDOM = XdmfDOM();
 
-	XdmfRoot * myRoot = new XdmfRoot();
-	myRoot->SetDOM(myDOM);
-	myRoot->Build();
+	XdmfRoot myRoot = XdmfRoot();
+	myRoot.SetDOM(&myDOM);
+	myRoot.Build();
 
-	XdmfDomain * myDomain = new XdmfDomain();
-	myRoot->Insert(myDomain);
+	XdmfDomain myDomain = XdmfDomain();
+	myRoot.Insert(&myDomain);
 
 	std::vector<int> intVector;
 	int intTotal = 100 ;
@@ -31,32 +32,44 @@ int main( int argc, const char* argv[] )
 		intVector.push_back(intTotal);
 	}
 
+	std::set<int> intSet;
+	intSet.insert(50);
+	intSet.insert(20);
+	intSet.insert(75);
+
 	std::map<int, double> myMap;
 	myMap[12]= 13.785412486;
 	myMap[60] = 45451.154987;
 	myMap[-60] = 5231.554874;
 
-	mySerializer->writeSetToXdmf(intVector, myDOM, "Vector");
-	mySerializer->writeMapToXdmf(myMap, myDOM, "Map");
+	// Just insert into grid for testing
+	XdmfGrid myGrid = XdmfGrid();
+	myGrid.SetGridType(XDMF_GRID_COLLECTION);
+	myDomain.Insert(&myGrid);
 
-	cout << myDOM->Serialize() << endl;
+	mySerializer.writeSetToXdmf(intVector, &myGrid, "Vector");
+	mySerializer.writeSetToXdmf(intSet, &myGrid, "Set");
+	mySerializer.writeMapToXdmf(myMap, &myGrid, "Map");
+
+	cout << myDOM.Serialize() << endl;
 
 	/*
 	 * Now attempt to do the reverse.  Read XDMF information into STL containers:
 	 */
 
-	XdmfXmlNode domain = myDOM->FindElement("Domain");
-	for (int i=0; i < myDOM->FindNumberOfElements("Set", domain); i++)
+	XdmfXmlNode domain = myDOM.FindElement("Domain");
+	XdmfXmlNode grid = myDOM.FindElement("Grid", 0, domain);
+	for (int i=0; i < myDOM.FindNumberOfElements("Set", grid); i++)
 	{
-	    XdmfSet * currSet = new XdmfSet();
-	    currSet->SetDOM(myDOM);
-	    currSet->SetElement(myDOM->FindElement("Set", i, domain));
-	    currSet->Update();
-	    if (currSet->GetNumberOfAttributes() == 0)
+	    XdmfSet currSet = XdmfSet();
+	    currSet.SetDOM(&myDOM);
+	    currSet.SetElement(myDOM.FindElement("Set", i, grid));
+	    currSet.Update();
+	    if (currSet.GetNumberOfAttributes() == 0)
 	    {
 	    	// Must be Non-Associative
 	    	std::vector<int> myData;
-	    	mySerializer->getSetFromXdmf(myData, currSet);
+	    	mySerializer.getSetFromXdmf(myData, &currSet);
 	    	cout << "\nVECTOR CONTAINS:" << endl;
 		    for (unsigned int j=0; j<myData.size(); j++)
 		    {
@@ -67,7 +80,7 @@ int main( int argc, const char* argv[] )
 	    {
 	    	// Must be Associative
 	    	std::map<int,double> myData;
-	    	mySerializer->getMapFromXdmf(myData, currSet);
+	    	mySerializer.getMapFromXdmf(myData, &currSet);
 	    	cout << "\nMAP CONTAINS:" << endl;
 	    	std::map<int,double>::iterator iter;
 	    	for (iter = myData.begin(); iter != myData.end(); iter++)
