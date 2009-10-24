789c0ffd30330188d66b2d3cecc0e670495b50ae
Dave Demarle <dave.demarle@kitware.com>
2009-10-23 20:05:47 -0400

    COMP: Fix unintentional disabliing of pointer sharing in writer.

--- vtk/vtkXdmfWriter2.cxx
+++ vtk/vtkXdmfWriter2.cxx
@@ -125,7 +125,7 @@ void vtkXdmfWriter2Internal::DetermineCellTypes(vtkPointSet * t, vtkXdmfWriter2I
 //==============================================================================
 
 vtkStandardNewMacro(vtkXdmfWriter2);
-vtkCxxRevisionMacro(vtkXdmfWriter2, "1.18");
+vtkCxxRevisionMacro(vtkXdmfWriter2, "1.19");
 
 //----------------------------------------------------------------------------
 vtkXdmfWriter2::vtkXdmfWriter2()
@@ -994,7 +994,7 @@ void vtkXdmfWriter2::ConvertVToXArray(vtkDataArray *vda,
 
   //TODO: if we can make xdmf write out immediately, then wouldn't have to keep around
   //arrays when working with temporal data
-  if (false)//(allocStrategy==0 && !this->TopTemporalGrid) || allocStrategy==1)
+  if ((allocStrategy==0 && !this->TopTemporalGrid) || allocStrategy==1)
     {
     //Do not let xdmf allocate its own buffer. xdmf just borrows vtk's and doesn't double mem size.
     xda->SetAllowAllocate(0); 
