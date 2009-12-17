7ca1528c2f29b5ac9b676cff09cac45e5b75343c
Kenneth Leiter <kenneth.leiter@arl.army.mil>
2009-12-17 13:17:32 -0500

    STYLE: Renamed files to maintain cxx extension like the rest of Xdmf.  Also made a change to XdmfDiff to write out all data to heavy format for more accurate comparisons.

--- libsrc/utils/XdmfFortran.h
+++ libsrc/utils/XdmfFortran.h
@@ -38,9 +38,9 @@ using std::endl;
 
 // Windows and DLL configuration
 #if defined(XdmfUtils_EXPORTS)
-    #define XDMF_UTILS_DLL __declspec(dllexport)
+  #define XDMF_UTILS_DLL __declspec(dllexport)
 #else
-    #define XDMF_UTILS_DLL __declspec(dllimport)
+  #define XDMF_UTILS_DLL __declspec(dllimport)
 #endif
 
 #else
@@ -50,55 +50,65 @@ using std::endl;
 
 #endif
 
-class XDMF_UTILS_DLL XdmfFortran{
+class XDMF_UTILS_DLL XdmfFortran
+{
 public:
-	XdmfFortran(char * outputName);
-	~XdmfFortran();
-	void SetTime(double * t);
-	void AddCollection(char * collectionType);
-	void CloseCollection();
-	void SetGridTopology(char * topologyType, int * numberOfElements, XdmfInt32 * conns);
-	void SetGridTopologyFromShape(char * topologyType, char * shape, XdmfInt32 * conns);
-	void SetGridGeometry(char * geometryType, char * numberType, int * numberOfPoints, XdmfPointer * points);
-	void AddGridAttribute(char * attributeName, char * numberType, char * attributeCenter, char * attributeType, int * numberOfPoints, XdmfPointer * data);
-        void AddGridAttributeFromShape(char * attributeName, char * numberType, char * attributeCenter, char * attributeType, char * shape, char * units, XdmfPointer * data);
-	void AddGridInformation(char * informationName, char * value);
-	void AddCollectionAttribute(char * attributeName, char * numberType, char * attributeCenter, char * attributeType, int * numberOfPoints, XdmfPointer * data);
-	void AddCollectionInformation(char * informationName, char * value);
-	void AddArray(char * name, char * numberType, int * numberOfValues, XdmfPointer * data);
-	void ReadFile(char * filePath);
-	void ReadGrid(char * gridName);
-	void ReadGridAtIndex(int * gridIndex);
-	void GetNumberOfPoints(XdmfInt32 * toReturn);
-	void GetNumberOfGrids(XdmfInt32 * toReturn);
-	void ReadPointValues(char * numberType, XdmfInt32 * startIndex, XdmfPointer * arrayToFill, XdmfInt32 * numberOfValues, XdmfInt32 * arrayStride, XdmfInt32 * valuesStride);
-	void GetNumberOfAttributeValues(char * attributeName, XdmfInt32 * toReturn);
-	void ReadAttributeValues(char * attributeName, char * numberType, XdmfInt32 * startIndex, XdmfPointer * arrayToFill, XdmfInt32 * numberOfValues, XdmfInt32 * arrayStride, XdmfInt32 * valuesStride);
-	void ReadInformationValue(char * informationName, char * valueToReturn);
-	void GetTime(XdmfFloat64 * toReturn);
-	void WriteGrid(char * gridName);
-	void WriteToFile();
-	void Serialize();
-	void GetDOM(char * charPointer);
+  XdmfFortran(char * outputName);
+  ~XdmfFortran();
+  void SetTime(double * t);
+  void AddCollection(char * collectionType);
+  void CloseCollection();
+  void SetGridTopology(char * topologyType, int * numberOfElements, XdmfInt32 * conns);
+  void SetGridTopologyFromShape(char * topologyType, char * shape, XdmfInt32 * conns);
+  void SetGridGeometry(char * geometryType, char * numberType, int * numberOfPoints,
+       XdmfPointer * points);
+  void AddGridAttribute(char * attributeName, char * numberType, char * attributeCenter,
+       char * attributeType, int * numberOfPoints, XdmfPointer * data);
+  void AddGridAttributeFromShape(char * attributeName, char * numberType, char * attributeCenter,
+       char * attributeType, char * shape, char * units, XdmfPointer * data);
+  void AddGridInformation(char * informationName, char * value);
+  void AddCollectionAttribute(char * attributeName, char * numberType, char * attributeCenter,
+       char * attributeType, int * numberOfPoints, XdmfPointer * data);
+  void AddCollectionInformation(char * informationName, char * value);
+  void AddArray(char * name, char * numberType, int * numberOfValues, XdmfPointer * data);
+  void ReadFile(char * filePath);
+  void ReadGrid(char * gridName);
+  void ReadGridAtIndex(int * gridIndex);
+  void GetNumberOfPoints(XdmfInt32 * toReturn);
+  void GetNumberOfGrids(XdmfInt32 * toReturn);
+  void ReadPointValues(char * numberType, XdmfInt32 * startIndex, XdmfPointer * arrayToFill,
+       XdmfInt32 * numberOfValues, XdmfInt32 * arrayStride, XdmfInt32 * valuesStride);
+  void GetNumberOfAttributeValues(char * attributeName, XdmfInt32 * toReturn);
+  void ReadAttributeValues(char * attributeName, char * numberType, XdmfInt32 * startIndex,
+       XdmfPointer * arrayToFill, XdmfInt32 * numberOfValues, XdmfInt32 * arrayStride,
+       XdmfInt32 * valuesStride);
+  void ReadInformationValue(char * informationName, char * valueToReturn);
+  void GetTime(XdmfFloat64 * toReturn);
+  void WriteGrid(char * gridName);
+  void WriteToFile();
+  void Serialize();
+  void GetDOM(char * charPointer);
 private:
-	void Destroy();
-	void ReadFilePriv(XdmfXmlNode currElement);
-	void ReadGridPriv(char * gridName, XdmfXmlNode currElement);
-	void ReadGridPriv(XdmfConstString gridPath);
-	void WriteToXdmfArray(XdmfArray * array, XdmfPointer * data);
-	void ReadFromXdmfArray(XdmfArray * array, char * numberType, XdmfInt32 * startIndex, XdmfPointer * arrayToFill,  XdmfInt32 * numberOfValues, XdmfInt32 * arrayStride, XdmfInt32 * valuesStride);
-	XdmfDOM * myDOM;
-	XdmfRoot * myRoot;
-	XdmfDomain * myDomain;
-	XdmfTopology * myTopology;
-	XdmfGeometry * myGeometry;
-	std::stack<XdmfGrid*> myCollections;
-	std::vector<XdmfAttribute*> myAttributes;
-	std::vector<XdmfInformation*> myInformations;
-	std::map<const char*, int> myGridNames;
-	std::vector<std::string> myGridPaths;
-	std::string myName;
-	double currentTime;
+  void Destroy();
+  void ReadFilePriv(XdmfXmlNode currElement);
+  void ReadGridPriv(char * gridName, XdmfXmlNode currElement);
+  void ReadGridPriv(XdmfConstString gridPath);
+  void WriteToXdmfArray(XdmfArray * array, XdmfPointer * data);
+  void ReadFromXdmfArray(XdmfArray * array, char * numberType, XdmfInt32 * startIndex,
+       XdmfPointer * arrayToFill, XdmfInt32 * numberOfValues, XdmfInt32 * arrayStride,
+       XdmfInt32 * valuesStride);
+  XdmfDOM * myDOM;
+  XdmfRoot * myRoot;
+  XdmfDomain * myDomain;
+  XdmfTopology * myTopology;
+  XdmfGeometry * myGeometry;
+  std::stack<XdmfGrid*> myCollections;
+  std::vector<XdmfAttribute*> myAttributes;
+  std::vector<XdmfInformation*> myInformations;
+  std::map<const char*, int> myGridNames;
+  std::vector<std::string> myGridPaths;
+  std::string myName;
+  double currentTime;
 };
 
 #endif /* XDMFFORTRAN_H_ */
