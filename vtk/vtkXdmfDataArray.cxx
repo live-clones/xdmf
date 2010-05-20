live-clones/xdmf

commit 61e337a60cf3bd4be2c79d6833358ee4c60baeb0

ENH: vtkXdmfReader to read attribute data with unexpected num components.
For example, we should be able to read 2D vectors into VTK by automatically appending a 0 to the Z component.  For attributes with a higher number of components than expected, split the attribute into it's scalar components for visualization.

vtk/vtkXdmfDataArray.cxx
@@ -2,9 +2,9 @@
 /*                               XDMF                              */
 /*                   eXtensible Data Model and Format              */
 /*                                                                 */
-/*  Id : Id  */
-/*  Date : $Date$ */
-/*  Version : $Revision$ */
+/*  Id : $Id: vtkXdmfDataArray.cxx,v 1.9 2009-07-23 21:23:22 kwleiter Exp $  */
+/*  Date : $Date: 2009-07-23 21:23:22 $ */
+/*  Version : $Revision: 1.9 $ */
 /*                                                                 */
 /*  Author:                                                        */
 /*     Jerry A. Clarke                                             */
@@ -38,7 +38,8 @@
 #include "vtkUnsignedShortArray.h"
 
 #include <XdmfArray.h>
-
+#include <XdmfDataItem.h>
+#include <XdmfDOM.h>
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkXdmfDataArray);
 
@@ -49,13 +50,12 @@ vtkXdmfDataArray::vtkXdmfDataArray()
   this->vtkArray = NULL;
 }
 
-
 //----------------------------------------------------------------------------
-vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
-  int rank, int Components, int MakeCopy ){
+vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int numVTKComponents,
+  int numXDMFComponents, int xdmfComponentStartIndex, int MakeCopy ){
   XdmfArray *array = this->Array;
-    XdmfInt64 components = 1;
-    XdmfInt64 tuples = 0;
+ // XdmfInt64 components = 1;
+  //XdmfInt64 tuples = 0;
   if ( ArrayName != NULL ) {
     array = TagNameToArray( ArrayName );
   }
@@ -118,104 +118,69 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
     vtkErrorMacro("Cannot create VTK data array: " << array->GetNumberType());
     return 0;
   }
-  if ( CopyShape )
+
+  this->vtkArray->SetNumberOfComponents(numVTKComponents);
+  int numComponentsToCopy = numVTKComponents;
+  if(numVTKComponents > numXDMFComponents)
+  {
+    this->vtkArray->SetNumberOfTuples(array->GetNumberOfElements() / numXDMFComponents);
+    for(int i=numXDMFComponents; i<numVTKComponents; ++i)
     {
-    if  ( array->GetRank() > rank + 1 )
-      {
-      this->vtkArray->Delete();
-      this->vtkArray = 0;
-      vtkErrorMacro("Rank of Xdmf array is more than 1 + rank of dataset");
-      return 0;
-      }
-    if ( array->GetRank() > rank ) 
-      {
-      components = array->GetDimension( rank );
-      }
-    tuples = array->GetNumberOfElements() / components;
-    /// this breaks
-    components = Components;
-    tuples = array->GetNumberOfElements() / components;
-     //cout << "Tuples: " << tuples << " components: " << components << endl;
-     //cout << "Rank: " << rank << endl;
-    this->vtkArray->SetNumberOfComponents( components );
-    if(MakeCopy) this->vtkArray->SetNumberOfTuples( tuples );
-    } 
-  else 
-    {
-    this->vtkArray->SetNumberOfComponents( 1 );
-    if(MakeCopy) this->vtkArray->SetNumberOfTuples( array->GetNumberOfElements() );
+      this->vtkArray->FillComponent(i, 0);
     }
-  //cout << "Number type: " << array->GetNumberType() << endl;
+    numComponentsToCopy = numXDMFComponents;
+  }
+  else
+  {
+    this->vtkArray->SetNumberOfTuples(array->GetNumberOfElements() / numXDMFComponents);
+  }
+
+  int numVTKValues = this->vtkArray->GetNumberOfComponents() * this->vtkArray->GetNumberOfTuples();
+  int xdmfStartIndex = xdmfComponentStartIndex;
+  int xdmfStride = numXDMFComponents;
+
+  // We can't take the pointer --- some rearranging must be done.
+  if(numVTKComponents != numXDMFComponents)
+  {
+    MakeCopy = 1;
+  }
+
   if(MakeCopy){
   switch( array->GetNumberType() ){
   case XDMF_INT8_TYPE :
-    array->GetValues( 0,
-      ( XDMF_8_INT*)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfInt8>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_UINT8_TYPE :
-    array->GetValues( 0,
-      ( XDMF_8_U_INT*)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfUInt8>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_INT16_TYPE :
-    array->GetValues( 0,
-      ( XDMF_16_INT*)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfInt16>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_UINT16_TYPE :
-    array->GetValues( 0,
-      ( XDMF_16_U_INT*)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfUInt16>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_INT32_TYPE :
-    array->GetValues( 0,
-      (XDMF_32_INT *)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfInt32>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_UINT32_TYPE :
-    array->GetValues( 0,
-      (XDMF_32_U_INT *)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfUInt32>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_INT64_TYPE :
-    array->GetValues( 0,
-      (XDMF_64_INT *)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfInt64>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_FLOAT32_TYPE :
-    array->GetValues( 0,
-      ( float *)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfFloat32>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   case XDMF_FLOAT64_TYPE :
-    array->GetValues( 0,
-      ( double *)this->vtkArray->GetVoidPointer( 0 ),
-      array->GetNumberOfElements() );  
+    this->CopyXdmfArray<XdmfFloat64>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
   default :
-    if ( array->GetNumberOfElements() > 0 )
-      {
-      //cout << "Manual idx" << endl;
-      //cout << "Tuples: " << vtkArray->GetNumberOfTuples() << endl;
-      //cout << "Components: " << vtkArray->GetNumberOfComponents() << endl;
-      //cout << "Elements: " << array->GetNumberOfElements() << endl;
-      vtkIdType jj, kk;
-      vtkIdType idx = 0;
-      for ( jj = 0; jj < vtkArray->GetNumberOfTuples(); jj ++ )
-        {
-        for ( kk = 0; kk < vtkArray->GetNumberOfComponents(); kk ++ )
-          {
-          double val = array->GetValueAsFloat64(idx);
-          //cout << "Value: " << val << endl;
-          vtkArray->SetComponent(jj, kk, val);
-          idx ++;
-          }
-        }
-      }
+    this->CopyXdmfArray<XdmfFloat64>(vtkArray, array, xdmfStartIndex, xdmfStride, numComponentsToCopy);
     break;
+    }
   }
-  }else{
+  else
+  {
   switch( array->GetNumberType() ){
   case XDMF_INT8_TYPE :
     {
@@ -224,7 +189,7 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast data array");
         return(0);
     }
-    chara->SetArray((char *)array->GetDataPointer(), components * tuples, 0);
+    chara->SetArray((char *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_UINT8_TYPE :
@@ -234,7 +199,7 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast ucharata array");
         return(0);
     }
-    uchara->SetArray((unsigned char *)array->GetDataPointer(), components * tuples, 0);
+    uchara->SetArray((unsigned char *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_INT16_TYPE :
@@ -244,17 +209,17 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast data array");
         return(0);
     }
-    shorta->SetArray((short *)array->GetDataPointer(), components * tuples, 0);
+    shorta->SetArray((short *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_UINT16_TYPE :
     {
-    vtkUnsignedShortArray *ushorta = vtkUnsignedShortArray::SafeDownCast(this->vtkArray);
-    if(!ushorta){
-        XdmfErrorMessage("Cannot downcast ushortata array");
+    vtkUnsignedCharArray *uchara = vtkUnsignedCharArray::SafeDownCast(this->vtkArray);
+    if(!uchara){
+        XdmfErrorMessage("Cannot downcast ucharata array");
         return(0);
     }
-    ushorta->SetArray((unsigned short *)array->GetDataPointer(), components * tuples, 0);
+    uchara->SetArray((unsigned char *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_INT32_TYPE :
@@ -264,7 +229,7 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast intata array");
         return(0);
     }
-    inta->SetArray((int *)array->GetDataPointer(), components * tuples, 0);
+    inta->SetArray((int *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_UINT32_TYPE :
@@ -274,7 +239,7 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast uintata array");
         return(0);
     }
-    uinta->SetArray((unsigned int *)array->GetDataPointer(), components * tuples, 0);
+    uinta->SetArray((unsigned int *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_INT64_TYPE :
@@ -284,7 +249,7 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast longa array");
         return(0);
     }
-    longa->SetArray((long *)array->GetDataPointer(), components * tuples, 0);
+    longa->SetArray((long *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_FLOAT32_TYPE :
@@ -294,7 +259,7 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast floatata array");
         return(0);
     }
-    floata->SetArray((float *)array->GetDataPointer(), components * tuples, 0);
+    floata->SetArray((float *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   case XDMF_FLOAT64_TYPE :
@@ -304,7 +269,7 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
         XdmfErrorMessage("Cannot downcast doubleata array");
         return(0);
     }
-    doublea->SetArray((double *)array->GetDataPointer(), components * tuples, 0);
+    doublea->SetArray((double *)array->GetDataPointer(), numVTKValues, 0);
     }
     break;
   default :
@@ -315,6 +280,24 @@ vtkDataArray *vtkXdmfDataArray::FromXdmfArray( char *ArrayName, int CopyShape,
   array->Reset();
   }
   return( this->vtkArray );
+}
+
+//----------------------------------------------------------------------------
+template <typename T>
+void vtkXdmfDataArray::CopyXdmfArray(vtkDataArray * vtkArray, XdmfArray * xdmfArray, int xdmfStartIndex, int xdmfStride, int numComponentsToCopy)
+{
+  int idx = xdmfStartIndex; 
+  for ( vtkIdType jj = 0; jj < vtkArray->GetNumberOfTuples(); jj ++ )
+    {
+    for ( vtkIdType kk = 0; kk < numComponentsToCopy; kk ++ )
+      {
+      T val;
+      xdmfArray->GetValues(idx+kk, &val, 1);
+      //cout << "Value: " << val << endl;
+      vtkArray->SetComponent(jj, kk, val);
+      }
+      idx+=xdmfStride;
+    }
 }
 
 //----------------------------------------------------------------------------

vtk/vtkXdmfDataArray.h
@@ -2,9 +2,9 @@
 /*                               XDMF                              */
 /*                   eXtensible Data Model and Format              */
 /*                                                                 */
-/*  Id : Id  */
-/*  Date : $Date$ */
-/*  Version : $Revision$ */
+/*  Id : $Id: vtkXdmfDataArray.h,v 1.3 2008-09-16 12:52:12 clarke Exp $  */
+/*  Date : $Date: 2008-09-16 12:52:12 $ */
+/*  Version : $Revision: 1.3 $ */
 /*                                                                 */
 /*  Author:                                                        */
 /*     Jerry A. Clarke                                             */
@@ -26,6 +26,7 @@
 #define _vtkXdmfDataArray_h
 
 #include <XdmfArray.h>
+#include <XdmfElement.h>
 #include <vtkDataArray.h>
 #include <vtkFloatArray.h>
 
@@ -42,8 +43,9 @@ public:
   char *ToArray( void ) {
     return( this->ToXdmfArray() );
     }
-  vtkDataArray *FromXdmfArray( char *ArrayName = NULL, int CopyShape = 1, 
-   int rank = 1, int Components = 1 , int MakeCopy = 1);
+  vtkDataArray *FromXdmfArray( char *ArrayName = NULL, int numVTKComponents = 1,
+    int numXDMFComponents = 1, int xdmfComponentStartIndex = 0, int MakeCopy = 1 );
+
   char *ToXdmfArray( vtkDataArray *DataArray = NULL, int CopyShape = 1 );
 
   void SetArray( char *TagName ) {
@@ -77,5 +79,6 @@ private:
   XdmfArray  *Array;
   vtkXdmfDataArray(const vtkXdmfDataArray&); // Not implemented
   void operator=(const vtkXdmfDataArray&); // Not implemented
+  template<typename T> void CopyXdmfArray(vtkDataArray * vtkArray, XdmfArray * xdmfArray, int xdmfStartIndex, int xdmfStride, int numComponentsToCopy);
 };
 #endif /* _vtkXdmfDataArray_h */

vtk/vtkXdmfHeavyData.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    vtkXdmfHeavyData.cxx
+  Module:    $RCSfile: vtkXdmfHeavyData.cxx,v $
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
@@ -38,6 +38,7 @@
 #include "vtkXdmfDataArray.h"
 #include "vtkXdmfReader.h"
 #include "vtkXdmfReaderInternal.h"
+#include "vtkXMLUnstructuredGridWriter.h"
 
 #include <vtkstd/deque>
 #include <assert.h>
@@ -350,7 +351,7 @@ int vtkXdmfHeavyData::GetNumberOfPointsPerCell(int vtk_cell_type)
   case VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON:
     return 24;
   case VTK_TRIQUADRATIC_HEXAHEDRON:
-    return 24;
+    return 27;
     }
   return -1;
 }
@@ -535,6 +536,12 @@ vtkDataObject* vtkXdmfHeavyData::ReadUnstructuredGrid(XdmfGrid* xmfGrid)
   ugData->SetPoints(points);
   points->Delete();
 
+  vtkXMLUnstructuredGridWriter * writer = vtkXMLUnstructuredGridWriter::New();
+  writer->SetDataModeToAscii();
+  writer->SetFileName("output.vtu");
+  writer->SetInput(ugData);
+  writer->Write();
+ 
   this->ReadAttributes(ugData, xmfGrid);
 
   // Read ghost cell/point information.
@@ -886,92 +893,120 @@ bool vtkXdmfHeavyData::ReadAttributes(
   for (int cc=0; cc < numAttributes; cc++)
     {
     XdmfAttribute* xmfAttribute = xmfGrid->GetAttribute(cc);
-    const char* attrName = xmfAttribute->GetName();
+    std::vector<std::string> attributeNames = this->Domain->GetAttributeNames(xmfGrid, cc);
     int attrCenter = xmfAttribute->GetAttributeCenter();
-    if (!attrName)
+    int i = 0;
+    int numXdmfComponents = attributeNames.size();
+    if(attributeNames.size() == 1)
+    {
+      numXdmfComponents = this->Domain->GetNumberOfComponents(xmfGrid, xmfAttribute); 
+    }
+    for(std::vector<std::string>::const_iterator iter = attributeNames.begin(); iter != attributeNames.end(); ++iter, ++i)
       {
-      vtkWarningWithObjectMacro(this->Reader,
-        "Skipping unnamed attributes.");
-      continue;
-      }
+      //if (!iter)
+      //  {
+      //  vtkWarningWithObjectMacro(this->Reader,
+      //    "Skipping unnamed attributes.");
+      //  continue;
+      //  }
 
-    vtkFieldData * fieldData = 0;
-    // skip disabled arrays.
-    switch (attrCenter)
-      {
-    case XDMF_ATTRIBUTE_CENTER_GRID:
-      fieldData = dataSet->GetFieldData();
-      break;
-
-    case XDMF_ATTRIBUTE_CENTER_CELL:
-      if (!this->Domain->GetCellArraySelection()->ArrayIsEnabled(attrName))
+      vtkFieldData * fieldData = 0;
+      // skip disabled arrays.
+      switch (attrCenter)
         {
-        continue;
-        }
-      fieldData = dataSet->GetCellData();
-      break;
+      case XDMF_ATTRIBUTE_CENTER_GRID:
+        fieldData = dataSet->GetFieldData();
+        break;
 
-    case XDMF_ATTRIBUTE_CENTER_NODE:
-      if (!this->Domain->GetPointArraySelection()->ArrayIsEnabled(attrName))
-        {
-        continue;
-        }
-      fieldData = dataSet->GetPointData();
-      break;
-
-    case XDMF_ATTRIBUTE_CENTER_FACE:
-    case XDMF_ATTRIBUTE_CENTER_EDGE:
-    default:
-      vtkWarningWithObjectMacro(this->Reader,
-        "Skipping attribute " << attrName << " at " <<
-        xmfAttribute->GetAttributeCenterAsString());
-      continue; // unhandled.
-      }
-
-    vtkDataArray* array = this->ReadAttribute(xmfAttribute,
-      data_dimensionality, update_extents);
-    if (array)
-      {
-      array->SetName(attrName);
-      fieldData->AddArray(array);
-      bool is_active = xmfAttribute->GetActive() != 0;
-      vtkDataSetAttributes* attributes =
-        vtkDataSetAttributes::SafeDownCast(fieldData);
-      if (attributes)
-        {
-        // make attribute active.
-        switch (xmfAttribute->GetAttributeType())
+      case XDMF_ATTRIBUTE_CENTER_CELL:
+        if (!this->Domain->GetCellArraySelection()->ArrayIsEnabled((*iter).c_str()))
           {
-        case XDMF_ATTRIBUTE_TYPE_SCALAR:
-          if (is_active || attributes->GetScalars() == NULL)
-            {
-            attributes->SetActiveScalars(attrName);
-            }
-          break;
+          continue;
+          }
+        fieldData = dataSet->GetCellData();
+        break;
 
-        case XDMF_ATTRIBUTE_TYPE_VECTOR:
-          if (is_active || attributes->GetVectors() == NULL)
-            {
-            attributes->SetActiveVectors(attrName);
-            }
-          break;
+      case XDMF_ATTRIBUTE_CENTER_NODE:
+        if (!this->Domain->GetPointArraySelection()->ArrayIsEnabled((*iter).c_str()))
+          {
+          continue;
+          }
+        fieldData = dataSet->GetPointData();
+        break;
 
-        case XDMF_ATTRIBUTE_TYPE_TENSOR:
-        case XDMF_ATTRIBUTE_TYPE_TENSOR6:
-          if (is_active || attributes->GetTensors() == NULL)
-            {
-            attributes->SetActiveTensors(attrName);
+      case XDMF_ATTRIBUTE_CENTER_FACE:
+      case XDMF_ATTRIBUTE_CENTER_EDGE:
+      default:
+        vtkWarningWithObjectMacro(this->Reader,
+          "Skipping attribute " << *iter << " at " <<
+          xmfAttribute->GetAttributeCenterAsString());
+        continue; // unhandled.
+        }
+      if(numXdmfComponents == 0)
+      {
+       vtkWarningWithObjectMacro(this->Reader,
+          "Skipping attribute " << xmfAttribute->GetName() << " which contains an incorrect number of values");
+       continue;
+      }
+      vtkDataArray* array;
+      if(attributeNames.size() == 1)
+      {
+        array = this->ReadAttribute(xmfAttribute, data_dimensionality, -1, numXdmfComponents, update_extents);
+      }
+      else
+      {
+        array = this->ReadAttribute(xmfAttribute, data_dimensionality, i, numXdmfComponents, update_extents);
+      }
+      if (array)
+        {
+        array->SetName((*iter).c_str());
+        fieldData->AddArray(array);
+        bool is_active = xmfAttribute->GetActive() != 0;
+        vtkDataSetAttributes* attributes =
+          vtkDataSetAttributes::SafeDownCast(fieldData);
+        if (attributes)
+          {
+          if(attributeNames.size() > 1)
+            { 
+            attributes->SetActiveScalars((*iter).c_str());
             }
-          break;
+          else
+           {
+            // make attribute active.
+            switch (xmfAttribute->GetAttributeType())
+              {
+            case XDMF_ATTRIBUTE_TYPE_SCALAR:
+              if (is_active || attributes->GetScalars() == NULL)
+                {
+                attributes->SetActiveScalars((*iter).c_str());
+                }
+              break;
 
-        case XDMF_ATTRIBUTE_TYPE_GLOBALID:
-          if (is_active || attributes->GetGlobalIds() == NULL)
-            {
-            attributes->SetActiveGlobalIds(attrName);
+            case XDMF_ATTRIBUTE_TYPE_VECTOR:
+              if (is_active || attributes->GetVectors() == NULL)
+                {
+                attributes->SetActiveVectors((*iter).c_str());
+                }
+              break;
+
+            case XDMF_ATTRIBUTE_TYPE_TENSOR:
+            case XDMF_ATTRIBUTE_TYPE_TENSOR6:
+              if (is_active || attributes->GetTensors() == NULL)
+                {
+                attributes->SetActiveTensors((*iter).c_str());
+                }
+              break;
+
+            case XDMF_ATTRIBUTE_TYPE_GLOBALID:
+              if (is_active || attributes->GetGlobalIds() == NULL)
+                {
+                attributes->SetActiveGlobalIds((*iter).c_str());
+                }
+              }
             }
           }
+        array->Delete();
         }
-      array->Delete();
       }
     }
   return true;
@@ -999,7 +1034,7 @@ void vtkConvertTensor6(T* source, T* dest, vtkIdType numTensors)
 
 //-----------------------------------------------------------------------------
 vtkDataArray* vtkXdmfHeavyData::ReadAttribute(XdmfAttribute* xmfAttribute,
-  int data_dimensionality, int* update_extents/*=0*/)
+  int data_dimensionality, int componentIndex, int numXdmfComponents, int* update_extents/*=0*/)
 {
   if (!xmfAttribute)
     {
@@ -1008,23 +1043,15 @@ vtkDataArray* vtkXdmfHeavyData::ReadAttribute(XdmfAttribute* xmfAttribute,
 
   int attrType = xmfAttribute->GetAttributeType();
   int attrCenter = xmfAttribute->GetAttributeCenter();
-  int numComponents = 1;
 
-  switch (attrType) 
-    {
-  case XDMF_ATTRIBUTE_TYPE_TENSOR :
-    numComponents = 9;
-    break;
-  case XDMF_ATTRIBUTE_TYPE_TENSOR6:
-    numComponents = 6;
-    break;
-  case XDMF_ATTRIBUTE_TYPE_VECTOR:
-    numComponents = 3;
-    break;
-  default :
-    numComponents = 1; 
-    break;
-    }
+  int numVTKComponents = 1;
+  
+  if(componentIndex < 0)
+  {
+    numVTKComponents = this->Domain->GetNumberOfExpectedComponents(xmfAttribute);
+    // Always start at 0
+    componentIndex = 0;
+  }
 
   XdmfDataItem xmfDataItem;
   xmfDataItem.SetDOM(xmfAttribute->GetDOM());
@@ -1084,10 +1111,11 @@ vtkDataArray* vtkXdmfHeavyData::ReadAttribute(XdmfAttribute* xmfAttribute,
     }
 
   vtkXdmfDataArray* xmfConvertor = vtkXdmfDataArray::New();
-  vtkDataArray* dataArray = xmfConvertor->FromXdmfArray(
-    xmfDataItem.GetArray()->GetTagName(), 1, data_rank, numComponents, 0);
-  xmfConvertor->Delete();
   
+  vtkDataArray* dataArray = xmfConvertor->FromXdmfArray(
+    xmfDataItem.GetArray()->GetTagName(), numVTKComponents, numXdmfComponents, componentIndex, 1);
+  xmfConvertor->Delete();
+
   if (attrType == XDMF_ATTRIBUTE_TYPE_TENSOR6)
     {
     // convert Tensor6 to Tensor.
@@ -1323,8 +1351,7 @@ vtkDataSet* vtkXdmfHeavyData::ExtractPoints(XdmfSet* xmfSet,
       {
       continue;
       }
-    vtkDataArray* array = this->ReadAttribute(xmfAttribute,
-      1, NULL);
+    vtkDataArray* array = this->ReadAttribute(xmfAttribute, 1, -1, 1, NULL);
     if (array)
       {
       array->SetName(attrName);
@@ -1402,7 +1429,7 @@ vtkDataSet* vtkXdmfHeavyData::ExtractCells(XdmfSet* xmfSet,
       {
       continue;
       }
-    vtkDataArray* array = this->ReadAttribute(xmfAttribute, 1, NULL);
+    vtkDataArray* array = this->ReadAttribute(xmfAttribute, 1, -1, 1, NULL);
     if (array)
       {
       array->SetName(attrName);
@@ -1491,7 +1518,7 @@ vtkDataSet* vtkXdmfHeavyData::ExtractFaces(XdmfSet* xmfSet, vtkDataSet* dataSet)
       {
       continue;
       }
-    vtkDataArray* array = this->ReadAttribute(xmfAttribute, 1, NULL);
+    vtkDataArray* array = this->ReadAttribute(xmfAttribute, 1, -1, 1, NULL);
     if (array)
       {
       array->SetName(attrName);
@@ -1591,7 +1618,7 @@ vtkDataSet* vtkXdmfHeavyData::ExtractEdges(XdmfSet* xmfSet, vtkDataSet* dataSet)
       {
       continue;
       }
-    vtkDataArray* array = this->ReadAttribute(xmfAttribute, 1, NULL);
+    vtkDataArray* array = this->ReadAttribute(xmfAttribute, 1, -1, 1, NULL);
     if (array)
       {
       array->SetName(attrName);

vtk/vtkXdmfHeavyData.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    vtkXdmfHeavyData.h
+  Module:    $RCSfile: vtkXdmfHeavyData.h,v $
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
@@ -20,6 +20,9 @@
 
 #include "XdmfDataItem.h"
 #include "XdmfGrid.h"
+
+#include <string>
+#include <vector>
 
 class vtkDataArray;
 class vtkDataObject;
@@ -121,8 +124,8 @@ private:
   // Reads an attribute.
   // If update_extents are non-null, then we are reading structured attributes
   // and we read only the sub-set specified by update_extents.
-  vtkDataArray* ReadAttribute(XdmfAttribute* xmfAttribute,
-    int data_dimensionality, int* update_extents=0);
+  vtkDataArray* ReadAttribute(XdmfAttribute* xmfAttribute, int data_dimensionality,
+    int componentIndex, int numXdmfComponents, int* update_extents=0);
 
   // Description:
   // Read sets that mark ghost cells/nodes and then create attribute arrays for

commit 04525f89b300b695f20792833afc8a4383c9e443

Remove old CVS keyword values
Commit 61e337a6 ("vtkXdmfReader to read attribute data with unexpected
num components", 2010-04-22) accidentally added the $Id$, $Date$,
$Revision$, and $RCSfile$ keyword values substituted by a CVS client.
These values had been removed during conversion from CVS.  The files in
this commit must have been copied from a CVS checkout.  We remove them
since they will not be updated automatically anymore.

vtk/vtkXdmfDataArray.cxx
@@ -2,9 +2,9 @@
 /*                               XDMF                              */
 /*                   eXtensible Data Model and Format              */
 /*                                                                 */
-/*  Id : $Id: vtkXdmfDataArray.cxx,v 1.9 2009-07-23 21:23:22 kwleiter Exp $  */
-/*  Date : $Date: 2009-07-23 21:23:22 $ */
-/*  Version : $Revision: 1.9 $ */
+/*  Id : Id                                                        */
+/*  Date : $Date$                                                  */
+/*  Version : $Revision$                                           */
 /*                                                                 */
 /*  Author:                                                        */
 /*     Jerry A. Clarke                                             */

vtk/vtkXdmfDataArray.h
@@ -2,9 +2,9 @@
 /*                               XDMF                              */
 /*                   eXtensible Data Model and Format              */
 /*                                                                 */
-/*  Id : $Id: vtkXdmfDataArray.h,v 1.3 2008-09-16 12:52:12 clarke Exp $  */
-/*  Date : $Date: 2008-09-16 12:52:12 $ */
-/*  Version : $Revision: 1.3 $ */
+/*  Id : Id                                                        */
+/*  Date : $Date$                                                  */
+/*  Version : $Revision$                                           */
 /*                                                                 */
 /*  Author:                                                        */
 /*     Jerry A. Clarke                                             */

vtk/vtkXdmfHeavyData.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkXdmfHeavyData.cxx,v $
+  Module:    vtkXdmfHeavyData.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.

vtk/vtkXdmfHeavyData.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkXdmfHeavyData.h,v $
+  Module:    vtkXdmfHeavyData.h
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.

vtk/vtkXdmfReaderInternal.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkXdmfReaderInternal.cxx,v $
+  Module:    vtkXdmfReaderInternal.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.

vtk/vtkXdmfReaderInternal.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkXdmfReaderInternal.h,v $
+  Module:    vtkXdmfReaderInternal.h
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
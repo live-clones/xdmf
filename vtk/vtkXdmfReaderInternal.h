04525f89b300b695f20792833afc8a4383c9e443
Brad King <brad.king@kitware.com>
2010-05-20 15:42:21 -0400

    Remove old CVS keyword values
    Commit 61e337a6 ("vtkXdmfReader to read attribute data with unexpected
    num components", 2010-04-22) accidentally added the $Id$, $Date$,
    $Revision$, and $RCSfile$ keyword values substituted by a CVS client.
    These values had been removed during conversion from CVS.  The files in
    this commit must have been copied from a CVS checkout.  We remove them
    since they will not be updated automatically anymore.

--- vtk/vtkXdmfReaderInternal.h
+++ vtk/vtkXdmfReaderInternal.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkXdmfReaderInternal.h,v $
+  Module:    vtkXdmfReaderInternal.h
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
