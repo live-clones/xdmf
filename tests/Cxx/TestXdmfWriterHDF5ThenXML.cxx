c061208736889d64b5396defec10d29b607bf562
Kenneth D. Renard (Cont ARL/CISD) kdrenard <kdrenard@area321-n03.arl.army.mil>
2010-09-07 07:44:47 -0400

    ENH: patch to fix problem encountered with "transparent" root object.

---
+++ tests/Cxx/TestXdmfWriterHDF5ThenXML.cxx
@@ -0,0 +1,34 @@
+#include "XdmfDomain.hpp"
+#include "XdmfHDF5Writer.hpp"
+#include "XdmfWriter.hpp"
+
+#include "XdmfTestDataGenerator.hpp"
+
+int main(int argc, char* argv[])
+{
+	boost::shared_ptr<XdmfGrid> grid = XdmfTestDataGenerator::createHexahedron();
+
+	// First write and release heavy data
+	boost::shared_ptr<XdmfHDF5Writer> hdf5Writer = XdmfHDF5Writer::New("output.h5");
+	grid->getGeometry()->accept(hdf5Writer);
+	grid->getGeometry()->release();
+
+	grid->getTopology()->accept(hdf5Writer);
+	grid->getTopology()->release();
+
+	for(int i=0; i<grid->getNumberAttributes(); ++i)
+	{
+		grid->getAttribute(i)->accept(hdf5Writer);
+		grid->getAttribute(i)->release();
+	}
+
+	// Now insert into domain and write light data
+	boost::shared_ptr<XdmfDomain> domain = XdmfDomain::New();
+	domain->insert(grid);
+
+	boost::shared_ptr<XdmfWriter> writer = XdmfWriter::New("output.xmf", hdf5Writer);
+	writer->setLightDataLimit(10);
+	domain->accept(writer);
+
+	return 0;
+}
