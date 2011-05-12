ce93f5928eef91b78a085b53157c957795c91463
Kenneth Leiter <kenneth.leiter@arl.army.mil>
2011-05-12 14:48:01 -0400

    ENH: Enable python test for XdmfSet.

--- tests/Python/TestXdmfSet.py
+++ tests/Python/TestXdmfSet.py
@@ -4,12 +4,13 @@ if __name__ == "__main__":
 
     set = XdmfSet.New()
     assert(set.getName() == "")
+    assert(not set.isInitialized())
     set.setName("foo")
     assert(set.getName() == "foo")
     assert(set.getType() == XdmfSetType.NoSetType())
     set.setType(XdmfSetType.Node())
     assert(set.getType() == XdmfSetType.Node())
     assert(set.getSize() == 0)
-    set.copyValueAsInt32(0, 1)
+    set.insertValueAsInt32(0, 1)
     assert(set.getSize() == 1)
 
