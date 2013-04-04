8858e32d276b5053ee958b6a98203fd592536c8b
Andrew J. Burns (Cont <andrew.j.burns2@us.army.mil>
2013-04-04 10:57:33 -0400

    Revert "changes to array to allow for functions"
    This reverts commit df13c70f06de94f5c6d827ed1edefcf09250f924.

--- core/XdmfCoreReader.cpp
+++ core/XdmfCoreReader.cpp
@@ -24,14 +24,11 @@
 #include <libxml/uri.h>
 #include <libxml/xpointer.h>
 #include <libxml/xmlreader.h>
-#include "boost/tokenizer.hpp"
 #include <map>
 #include <sstream>
 #include <cstring>
 #include <utility>
 #include "XdmfArray.hpp"
-#include "XdmfArrayType.hpp"
-#include "XdmfHDF5Controller.hpp"
 #include "XdmfCoreItemFactory.hpp"
 #include "XdmfCoreReader.hpp"
 #include "XdmfError.hpp"
@@ -171,317 +168,6 @@ public:
           }
 
         }
-        else if (xmlStrcmp(currNode->name, (xmlChar*)"Function") == 0) {
-          //function handling goes here
-          xmlNodePtr childNode = currNode->children;
-
-          std::string arraySubType = "";
-          //get Array Subtype, if any
-          xmlAttrPtr currAttribute = currNode->properties;
-          while (currAttribute != NULL) {
-            if (xmlStrcmp(currAttribute->name, (xmlChar*)"Type") == 0 ) {
-              arraySubType = (char*)currAttribute->children->content;
-              break;//uses the first type found
-            }
-          }
-
-          std::string expressionToParse = "";
-
-          while (childNode != NULL) {
-            if (xmlStrcmp(childNode->name, (xmlChar*)"Expression") == 0){
-              //store expression
-              xmlAttrPtr childAttribute = childNode->properties;
-              while (childAttribute != NULL) {
-                if(xmlStrcmp(childAttribute->name, (xmlChar*)"Value") == 0) {
-                  expressionToParse = (char*)childAttribute->children->content;
-                  break;
-                }
-                childAttribute = childAttribute->next;
-              }
-              break;
-            }
-            childNode = childNode->next;
-          }
-
-          if (expressionToParse.compare("") == 0) {
-            XdmfError::message(XdmfError::FATAL,
-              "Error: No Expression in Function");
-          }
-
-          //two seperate loops to allow for different orders and multiple variable sets
-          childNode = currNode->children;
-
-          std::map<std::string, shared_ptr<XdmfArray> > variableCollection;
-          while (childNode != NULL) {
-            if (xmlStrcmp(childNode->name, (xmlChar*)"Variable") == 0) {
-              //store child variables
-              xmlNodePtr childVariable = childNode->children;
-
-              while (childVariable != NULL) {
-                if (xmlStrcmp(childVariable->name, (xmlChar*)"DataItem") == 0) {
-                  xmlAttrPtr childAttribute = childVariable->properties;
-                  std::string childKey = "";
-                  shared_ptr<XdmfArray> childArray = XdmfArray::New();
-                  std::string dataString = "";
-                  shared_ptr<const XdmfArrayType> dataType;
-                  xmlChar * childXPointer = NULL;
-                  xmlChar * childhref = NULL;
-                  std::string childhdf5 = "";
-                  std::map<std::string, std::string> typeMap;
-
-                  while (childAttribute != NULL) {
-                    //the variable type of the array
-                    if (xmlStrcmp(childAttribute->name, (xmlChar*)"DataType") == 0) {
-                      typeMap["DataType"] = (char*)childAttribute->children->content;
-                    }
-                    //the precision of the variable type (only used for long and double)
-                    else if (xmlStrcmp(childAttribute->name, (xmlChar*)"Precision") == 0) {
-                      typeMap["Precision"] = (char*)childAttribute->children->content;
-                    }
-                    //the key or mapped string for the variable
-                    else if (xmlStrcmp(childAttribute->name, (xmlChar*)"Key") == 0) {
-                      childKey = (char*)childAttribute->children->content;
-                    }
-                    //text based xml data
-                    else if (xmlStrcmp(childAttribute->name, (xmlChar*)"Value") == 0) {
-                      dataString = (char*)childAttribute->children->content;
-                    }
-                    //an x pointer to another XdmfArray
-                    else if (xmlStrcmp(childAttribute->name, (xmlChar*)"XPointer") == 0) {
-                      childXPointer = childAttribute->children->content;
-                    }
-                    //used in conjunction with Xpointers to reference objects in a different file
-                    else if (xmlStrcmp(childAttribute->name, (xmlChar*)"href") == 0) {
-                      childhref = childAttribute->children->content;
-                    }
-                    //path to hdf5 data sets and the dimensions of those sets
-                    else if (xmlStrcmp(childAttribute->name, (xmlChar*)"hdf5") == 0) {
-                      childhdf5 = (char*)childAttribute->children->content;
-                    }
-                    childAttribute = childAttribute->next;
-                  }
-
-                  if (typeMap["DataType"].compare("Float") == 0) {
-                    if (typeMap["Precision"].compare("8") == 0) {
-                      dataType = XdmfArrayType::Float64();
-                    }
-                    else {
-                      dataType = XdmfArrayType::Float32();
-                    }
-                  }
-                  else if (typeMap["DataType"].compare("Int") == 0) {
-                    if (typeMap["Precision"].compare("8") == 0) {
-                      dataType = XdmfArrayType::Int64();
-                    }
-                    else {
-                      dataType = XdmfArrayType::Int32();
-                    }
-                  }
-                  else if (typeMap["DataType"].compare("String") == 0) {
-                    dataType = XdmfArrayType::String();
-                  }
-                  else if (typeMap["DataType"].compare("Char") == 0) {
-                    dataType = XdmfArrayType::Int8();
-                  }
-                  else if (typeMap["DataType"].compare("Short") == 0) {
-                    dataType = XdmfArrayType::Int16();
-                  }
-                  else if (typeMap["DataType"].compare("UChar") == 0) {
-                    dataType = XdmfArrayType::UInt8();
-                  }
-                  else if (typeMap["DataType"].compare("UShort") == 0) {
-                    dataType = XdmfArrayType::UInt16();
-                  }
-                  else if (typeMap["DataType"].compare("UInt") == 0) {
-                    dataType = XdmfArrayType::UInt32();
-                  }
-                  else {
-                    dataType = XdmfArrayType::Uninitialized();
-                  }
-
-                  //if xpointer grab item at that location
-                  if (childXPointer) {
-                    xmlXPathContextPtr context = mXPathContext;
-
-                    if(childhref) {
-                      xmlDocPtr document;
-                      xmlChar * filePath = xmlBuildURI(childhref, mDocument->URL);
-                      std::map<std::string, xmlDocPtr>::const_iterator iter =
-                        mDocuments.find((char*)filePath);
-                      if(iter == mDocuments.end()) {
-                        document = xmlReadFile((char*)filePath, NULL, 0);
-                        mDocuments.insert(std::make_pair((char*)document->URL, document));
-                      }
-                      else {
-                        document = iter->second;
-                      }
-                      context = xmlXPtrNewContext(document, NULL, NULL);
-                    }
-
-                    if(childXPointer) {
-                      xmlXPathObjectPtr result = xmlXPtrEval(childXPointer, context);
-                      if(result && !xmlXPathNodeSetIsEmpty(result->nodesetval)) {
-                        for(int i=0; i<result->nodesetval->nodeNr; ++i) {
-                          //there should only be one item being returned here
-                          //place into a new vector
-                          std::vector<shared_ptr<XdmfItem> > pointedItems;
-                          this->readSingleNode(result->nodesetval->nodeTab[i], pointedItems);
-                          try {
-                            childArray = shared_dynamic_cast<XdmfArray>(pointedItems[0]);//try to cast it as an array
-                          }
-                          catch (...) {//if that doesn't work throw an error
-                            XdmfError::message(XdmfError::FATAL,
-                                               "Error: Variable not Equivalent to an Array");//because we should only be working with arrays
-                          }
-                        }
-                      }
-                      xmlXPathFreeObject(result);
-                    }
-
-                    if(childhref) {
-                      xmlXPathFreeContext(context);
-                    }
-                  }
-                  //if hdf5 create controllers and attach it
-                  else if (childhdf5.compare("") != 0) {
-                    //parse the hdf5 controllers
-                    std::vector<std::string> controllerParts;
-                    //split the content based on "|" characters
-                    size_t barSplit = 0;
-                    std::string splitString(childhdf5);
-                    std::string subcontent;
-
-                    while (barSplit != std::string::npos) {
-                      barSplit = 0;
-                      barSplit = splitString.find_first_of("|", barSplit);
-                      if (barSplit == std::string::npos) {
-                        subcontent = splitString;
-                      }
-                      else {
-                        subcontent = splitString.substr(0, barSplit);
-                        splitString = splitString.substr(barSplit+1);
-                        barSplit++;
-                      }
-                      controllerParts.push_back(subcontent);
-                    }
-
-                    //insert those controllers into the childArray
-                    int hdf5step = 2;
-                    for (int i = 0; i < controllerParts.size(); i = i + hdf5step) {
-                      size_t colonLocation = controllerParts[i].find(":");
-                      if(colonLocation == std::string::npos) {
-                        XdmfError::message(XdmfError::FATAL,
-                                           "':' not found in function variable content in "
-                                           "read -- double check an HDF5 "
-                                           "data set is specified for the file");
-                      }
-
-                      std::string hdf5Path = controllerParts[i].substr(0, colonLocation);
-                      std::string dataSetPath = controllerParts[i].substr(colonLocation+1);
-                      std::vector<unsigned int> contentDims;
-
-			//TODO might need to rewrite this try-catch block
-                      try {
-                        //this is the string that contains the dimensions
-                        boost::tokenizer<> dimtokens(controllerParts[i + 1]);
-                        for(boost::tokenizer<>::const_iterator iter = dimtokens.begin();
-                            iter != dimtokens.end();
-                            ++iter) {
-                          contentDims.push_back(atoi((*iter).c_str()));
-                        }
-                        hdf5step = 2;//if this works then the dimension content should be skipped over
-                      }
-                      catch (...) {//if it fails then it means that the next content is not a dimension string
-                        //in this case an error should be thrown, formatting error
-                        //because there is no base array to pull dimensions from
-                        XdmfError::message(XdmfError::FATAL,
-                                           "Error: Improper HDF5 Format");
-                      }
-
-                      childArray->insert(XdmfHDF5Controller::New(hdf5Path,
-                                                                 dataSetPath,
-                                                                 dataType,
-                                                                 std::vector<unsigned int>(contentDims.size(), 0),
-                                                                 std::vector<unsigned int>(contentDims.size(), 1),
-                                                                 contentDims,
-                                                                 contentDims));
-                    }
-                  }
-                  //if xml parse strait to insert
-                  else if (dataString.compare("") != 0) {
-                    //parse the data into tokens
-                    childArray->initialize(dataType, 0);
-                    unsigned int index = 0;
-                    boost::char_separator<char> sep(" \t\n");
-                    boost::tokenizer<boost::char_separator<char> > tokens(dataString, sep);
-                    if(dataType == XdmfArrayType::String()) {
-                      for(boost::tokenizer<boost::char_separator<char> >::const_iterator
-                          iter = tokens.begin();
-                          iter != tokens.end();
-                          ++iter, ++index) {
-                        //insert those tokens into the childArray
-                        childArray->insert(index, *iter);
-                      }
-                    }
-                    else {
-                      for(boost::tokenizer<boost::char_separator<char> >::const_iterator
-                          iter = tokens.begin();
-                          iter != tokens.end();
-                          ++iter, ++index) {
-                        //insert those tokens into the childArray
-                        childArray->insert(index, atof((*iter).c_str()));
-                      }
-                    }
-                  }
-
-                  //parse the value into the array
-                  if (childKey.compare("") != 0){
-                    if (variableCollection.find(childKey) != variableCollection.end()) {
-                      XdmfError::message(XdmfError::WARNING,
-                                         "Warning: Variable Redefined");
-                    }
-                    childArray->read();
-                    variableCollection[childKey] = childArray;
-                  }
-                  else {
-                    XdmfError::message(XdmfError::WARNING,
-                                       "Warning: Value Unpaired to Key");
-                  }
-                }
-                childVariable = childVariable->next;
-              }
-            }
-            childNode = childNode->next;
-          }
-
-          shared_ptr<XdmfArray> parsedArray = XdmfArray::evaluateExpression(expressionToParse, variableCollection);
-          //the properties and children aren't really needed to generate the object, but the factory still requires them.
-          std::map<std::string, std::string> newArrayProperties;
-          std::vector<shared_ptr<XdmfItem> > newArrayChildren;
-          shared_ptr<XdmfArray> returnArray = XdmfArray::New();
-
-          if (arraySubType.compare("") == 0) {//if no type is specified an array is generated
-            arraySubType = "DataItem";
-          }
-
-          //this should generate an item that corresponds to the tag provided, the casting ensures that it is a subtype of array
-          //using a factory to be able to build things outside of core
-          returnArray = shared_dynamic_cast<XdmfArray>(mItemFactory->createItem(
-                                                       arraySubType,
-                                                       newArrayProperties,
-                                                       newArrayChildren));
-
-          if (!returnArray) {//if the specified tag fails to generate an item then reclass as an array
-            arraySubType = "DataItem";
-            returnArray = shared_dynamic_cast<XdmfArray>(mItemFactory->createItem(
-                                                         arraySubType,
-                                                         newArrayProperties,
-                                                         newArrayChildren));
-          }
-
-          returnArray->insert(0, parsedArray, 0, parsedArray->getSize());
-          myItems.push_back(returnArray);
-        }
         else {
           // Normal reading
           this->readSingleNode(currNode, myItems);
@@ -503,13 +189,13 @@ public:
                  std::vector<shared_ptr<XdmfItem> > & myItems)
   {
 
-    //check to see if the node is already in the Xpath
+
     std::map<xmlNodePtr, shared_ptr<XdmfItem> >::const_iterator iter =
       mXPathMap.find(currNode);
-    if(iter != mXPathMap.end()) {//if it is grab it from the previously stored items
+    if(iter != mXPathMap.end()) {
       myItems.push_back(iter->second);
     }
-    else {//otherwise, generate it from the node
+    else {
       std::map<std::string, std::string> itemProperties;
 
       xmlNodePtr childNode = currNode->children;
@@ -563,6 +249,7 @@ public:
             }
           }
           childNode = childNode->next;
+          //childContentIndex++;
         }
       }
     
